6f523cbc894789880db47a0a7e0b9ac7227bdb83
ENGRODAH313 <coe.22.212@student.uotechnology.edu.iq>
2024-08-17 20:14:07 +0300

    Add files via upload

---
+++ CODE.ino
@@ -0,0 +1,15 @@
+//Arduino Circuit Design Course 
+ // Project Title : LED CIRUCIT
+  //Done by::mostafa hussin rashid
+void setup()
+{
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void loop()
+{
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(1000); // Wait for 1000 millisecond(s)
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(1000); // Wait for 1000 millisecond(s)
+}
